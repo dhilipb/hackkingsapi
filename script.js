5cac40f9b3dc4dff3b13dcd716acf81784560bfc
dhilipb <dhilipb@gmail.com>
2015-12-11 14:30:25 +0000

    Updating with latest version

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
+// Code goes here
+
 jQuery(document).ready(function() {
-	var $ = jQuery;
-	$("body").on("click", ".nav a", function() {
-		var parent = $(this).parents(".lang-samples");
-		parent.find(".nav .active").removeClass("active");
+    var $ = jQuery;
+    $("body").on("click", ".nav a", function() {
+        var parent = $(this).parents(".lang-samples");
+        parent.find(".nav .active").removeClass("active");
         $(this).parents("li").addClass("active");
         parent.find(".tab-pane.active").removeClass("active");
-		parent.find(".tab-pane." + $(this).data("tab-id")).addClass("active");
-	}).on("click", "h2", function() {
-		var api_section = $(this).parents(".api-section");
-		api_section.find(".api-content").toggleClass("expand");
-		api_section.find(".expand-collapse-icon").toggleClass("expand");
-	});
+        parent.find(".tab-pane." + $(this).data("tab-id")).addClass("active");
+    }).on("click", "h2", function() {
+        var api_section = $(this).parents(".api-section");
+        api_section.find(".api-content").toggleClass("expand");
+        api_section.find(".expand-collapse-icon").toggleClass("expand");
+    });
 
-	$.get("https://raw.githubusercontent.com/dhilipb/hackkingsapi/master/portfolio-analysis.html", function(html) {
-		if (html != $("#api-portfolio-analysis").html()) {
-			$("#api-portfolio-analysis").html(html);
-		}
-	});
-	$.get("https://raw.githubusercontent.com/dhilipb/hackkingsapi/master/performance-data.html", function(html) {
-		if (html != $("#api-performance-data").html()) {
-			$("#api-performance-data").html(html);
-		}
-	});
-	$.get("https://raw.githubusercontent.com/dhilipb/hackkingsapi/master/search-securities.html", function(html) {
-		if (html != $("#api-search-securities").html()) {
-			$("#api-search-securities").html(html);
-		}
-	});
-	$.get("https://raw.githubusercontent.com/dhilipb/hackkingsapi/master/security-data.html", function(html) {
-		if (html != $("#api-security-data").html()) {
-			$("#api-security-data").html(html);
-		}
-	});
-
+    $.get("https://rawgit.com/dhilipb/hackkingsapi/master/portfolio-analysis.html", function(response) {
+        $("#api-portfolio-analysis").html(response);
+    });
+    $.get("https://rawgit.com/dhilipb/hackkingsapi/master/performance-data.html", function(response) {
+        $("#api-performance-data").html(response);
+    });
+    $.get("https://rawgit.com/dhilipb/hackkingsapi/master/search-securities.html", function(response) {
+        $("#api-search-securities").html(response);
+    });
+    $.get("https://rawgit.com/dhilipb/hackkingsapi/master/security-data.html", function(response) {
+        $("#api-security-data").html(response);
+    });
 });
-
